b5feb7c18ec7be3019b93801ad6124e86afba7b7
Takahiro Nomoto <nmt.1727@gmail.com>
2024-04-18 17:48:10 +0900

    食事記録をマイページのフォームからできるようにした

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -5,8 +5,23 @@ class MealsController < ApplicationController
   # def new; end
 
   def create
-    
-
+    user_id = current_user.id
+    meal = Meal.find_by(meal_name: params[:meal_name])
+    if meal.present?
+      meal_id = meal.id
+    else
+      meal = Meal.new(meal_name: params[:meal_name], user_id: user_id)
+      meal.save
+      meal_id = meal.id
+    end
+    evaluation = Evaluation.new(user_id: user_id, meal_id: meal_id, eated_at: params[:eated_at])
+    if evaluation.save
+      flash[:notice] = '食事を記録しました'
+      redirect_to user_path(current_user)
+    else
+      flash.now[:danger] = '食事の記録に失敗しました'
+      render("user/show")
+    end
   end
 
   # def show; end
