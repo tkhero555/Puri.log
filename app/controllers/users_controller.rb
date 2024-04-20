1bcdfe7c87ba1ba3c8aa3a8f185904bea6f9c08c
Takahiro Nomoto <nmt.1727@gmail.com>
2024-04-20 15:09:44 +0900

    相性判定機能　linebotは一旦コメントアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def show; end
+  def show
+    @recommend_meals = current_user.evaluations.includes(:meal).where('score >= ?', 3)
+    @avert_meals = current_user.evaluations.includes(:meal).where('score <= ?', -3)
+  end
 end
