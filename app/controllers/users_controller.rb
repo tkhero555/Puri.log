8d79afe8d3705bab56f5b2fe4d2818ce001f6ba8
Takahiro Nomoto <nmt.1727@gmail.com>
2024-04-26 22:56:54 +0900

    マイページのレイアウトができた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,18 @@ class UsersController < ApplicationController
     if @avert_meals.nil?
       @avert_meals = current_user.meals.where('score >= ?', 1)
     end
+
+    meal_log_index = current_user.eatings.includes(:meal)
+    stool_log_index = current_user.stools
+    meal_log_index = meal_log_index.to_a
+    stool_log_index = stool_log_index.to_a
+    @log_index = meal_log_index.concat(stool_log_index)
+    @log_index = @log_index.sort_by do |log|
+      if log.is_a?(Eating)
+        log.eated_at
+      elsif log.is_a?(Stool)
+        log.created_at
+      end
+    end
   end
 end
