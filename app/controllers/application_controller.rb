9f1d18d5527c15bd38d2bb66e49c854d80c0282f
Takahiro Nomoto <nmt.1727@gmail.com>
2024-05-17 22:35:26 +0900

    pメソッドが残ってた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,22 +10,17 @@ class ApplicationController < ActionController::Base
     if params[:desc]
       @log_index = @log_index.sort_by { |log| log.created_at }.reverse
       session[:sort] = "desc"
-      p "params descで判定"
     elsif params[:asc]
       @log_index = @log_index.sort_by { |log| log.created_at }
       session[:sort] = "asc"
-      p "params ascで判定"
     else
       if session[:sort] == "desc"
         @log_index = @log_index.sort_by { |log| log.created_at }.reverse
         session[:sort] = "desc"
-        p "session descで判定"
       elsif session[:sort] == "asc"
         @log_index = @log_index.sort_by { |log| log.created_at }
         session[:sort] = "asc"
-        p "session descで判定"
       end
     end
-    p session[:sort]
   end
 end
