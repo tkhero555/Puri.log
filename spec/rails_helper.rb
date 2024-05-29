e486625b0b79cefe46b842fbd79b6e335d53eee4
Takahiro Nomoto <nmt.1727@gmail.com>
2024-05-30 08:02:03 +0900

    fix:linebotコントローラーの整備

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -83,4 +83,11 @@ RSpec.configure do |config|
   config.before(:each, type: :system, js: true) do
     driven_by :selenium_chrome_headless
   end
+
+  if ENV['CIRCLE_ARTIFACTS']
+    dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+    SimpleCov.coverage_dir(dir)
+  end
+
+  SimpleCov.start
 end
