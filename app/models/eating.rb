27bd61640b4b799f5cdcc57eec0425d188d36273
Takahiro Nomoto <nmt.1727@gmail.com>
2024-05-31 13:35:28 +0900

    fix:定数をモデルファイルに移動&lin_bot_controllerにコメントを追加

--- app/models/eating.rb
+++ app/models/eating.rb
@@ -4,4 +4,9 @@ class Eating < ApplicationRecord
 
   scope :this_month, -> { where(created_at: Time.current.beginning_of_month..Time.current.end_of_month) }
   scope :last_month, -> { where(created_at: Time.current.last_month.beginning_of_month..Time.current.last_month.end_of_month) }
+
+  # Eatingモデルに関連する定数の設定
+  # 排便の原因となる食事を特定する時間範囲
+  DETERMINING_COMPATIBILITY_START_TIME = 20
+  DETERMINING_COMPATIBILITY_END_TIME = 50
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,4 +2,16 @@ class Meal < ApplicationRecord
   belongs_to :user
   has_many :eatings, dependent: :destroy
   validates :meal_name, presence: true, length: { maximum: 30 }
+
+  # Mealモデルに関連する定数の設定
+  # 食事score変動用
+  MEAL_SCORE_CHANGE_ZERO = 0
+  MEAL_SCORE_CHANGE_PLUS = 1
+  MEAL_SCORE_CHANGE_MINUS = 2
+
+  # おすすめの食事と避けるべき食事の判定基準の値
+  RECOMMEND_MEAL_JUDGE_FIRST_POINT = 3
+  RECOMMEND_MEAL_JUDGE_SECOND_POINT = 1
+  AVERT_MEAL_JUDGE_FIRST_POINT = -3
+  AVERT_MEAL_JUDGE_SECOND_POINT = -1
 end
