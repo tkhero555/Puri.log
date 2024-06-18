1a84b5c9fce622963801221aa6a0e414b87d5f0f
Takahiro Nomoto <nmt.1727@gmail.com>
2024-06-18 17:54:04 +0900

    fix:オートコンプリートが動作しなくなっていたバグを修正

--- config/routes.rb
+++ config/routes.rb
@@ -18,12 +18,14 @@ Rails.application.routes.draw do
 
   resource :user, only: %i[show destroy] do
     collection do
-      get :search
       post :sort, as: 'sort_log'
       post :toggle_notifications
       post :accept_terms
     end
   end
+  # stimulus_autocompleteの要求urlに合わせるために個別に設定
+  get 'users/search', to: 'users#search'
+
   resources :meals, only: %i[create]
   resources :stools, only: %i[create destroy]
   resources :eatings, only: %i[destroy]
